05d5f884ad1918ce7fae06486a57921cafadc620
Ubaid9513 <shaikhubaid2345@gmail.com>
2024-04-01 03:51:50 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,65 @@
+// function foo(){
+//     console.log("Ubaid");
+// }
+// foo();
+// foo();
+// foo();
+
+
+// Passing them data
+
+// function foo(name){
+//     console.log(name);
+// }
+// foo("Ubaid Aslam");
+// foo("Ubaid");
+
+
+// function foo(a,b,c){
+//         console.log(a,b,c);
+//     }
+//     foo("Ubaid","Aslam","Taha");
+
+// ---X---
+
+// var arr = [];
+
+// function foo(value){
+//     arr[arr.length] = value
+// }
+// foo ("Ubaid");
+// foo ("Aslam");
+
+// console.log(arr);
+
+
+// function printName(name){
+//     if(name){
+//         document.write(name);
+//     }
+// }
+// printName("Ubaid");
+
+// function printName(name){
+//         document.write(name);
+// }
+// printName(prompt("Enter your Name"));
+
+
+// function abc(a,b){
+//     return a * b;
+// }
+// console.log(abc(10,10));
+
+// function abc(a,b){
+//     return a + b;
+// }
+// var result = abc(10,10);
+// console.log(result);
+
+function abc(a,b){
+    var output = a + b;
+    return output;
+}
+var result = abc(10,10);
+console.log(result);
