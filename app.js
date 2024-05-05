a936d4f4cadc9c718565d8ce6e8bb87ad017eccd
Ubaid9513 <shaikhubaid2345@gmail.com>
2024-05-06 00:58:09 +0500

    Updated

--- app.js
+++ app.js
@@ -78,3 +78,71 @@
 // Ans. Var FUNCTION SCOPE h.Agar VAR function k andar bnaya gya ho toh wo function k andar hi accessable hoga.
 // Agar apko function k bahir uski value chahiye toh return krna hoga.
 
+
+// ---X---
+
+// function callAlert(props, value) {
+//   console.log(props.name, props.phone);
+
+// for (let i = 0; i < value.length; i++) {
+//   const element = value[i];
+//   console.log(element);
+// }
+// }
+
+// function sum(num1, num2) {
+//   console.log(num1 + num2);
+// }
+
+// function square(num) {
+//   return num * num;
+//   if (num > 5) {  // function can only return once, after return code will not run
+//     console.log();
+//   }
+// }
+// console.log(square(3));
+// console.log(square(4));
+// alert(square(5));
+
+// var products = [
+//   { name: "banana", price: 20, isDisCounted: true },
+//   { name: "apple", price: 40, isDisCounted: true },
+//   { name: "mango", price: 50, isDisCounted: false },
+// ];
+// function applyDiscount(price) {
+//   var discount = (price * 50) / 100;
+//   return discount;
+// }
+
+// for (var i = 0; i < products.length; i++) {
+//   if (products[i].isDisCounted) {
+//     document.writeln(
+//       "<p>" +
+//         products[i].name +
+//         ": was <s>" +
+//         products[i].price +
+//         "</s>" +
+//         "now" +
+//         applyDiscount(products[i].price),
+//       "</p>"
+//     );
+//   } else {
+//     document.writeln(
+//       "<p>" + products[i].name + ": " + products[i].price,
+//       "</p> "
+//     );
+//   }
+// }
+
+
+// ---X---
+
+function xyz(){
+  var a = 5;
+
+  function abc(){
+    console.log(a);
+  }
+  abc();
+}
+xyz();
